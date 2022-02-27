75022e2ee51054ae1a7553bf1eb1d1b62ea9afa1
mj-lopes <mlrj.junior@gmail.com>
2022-02-27 19:17:38 -0300

    Header reestilizado

--- src/components/Header/style.ts
+++ src/components/Header/style.ts
@@ -1,24 +1,27 @@
 import { createStyles } from "@mantine/core";
 
-const CORBG = "rgba(0, 43, 66, 0.8)";
+const CORBG = "rgba(0, 43, 66, 0.95)";
 
 const useStyles = createStyles((theme, params, getRef) => {
   return {
     background: {
       zIndex: 1000,
-      height: "80px",
+      height: "70px",
+      // 80
       backgroundColor: CORBG,
       position: "fixed",
       width: "100%",
     },
     semiCirculo: {
       position: "relative",
-      bottom: "-80px",
+      bottom: "-70px",
+      // -80
       margin: "auto",
 
-      height: "60px",
-      maxWidth: "130px",
-
+      height: "50px",
+      // 60
+      maxWidth: "120px",
+      // 110
       background: CORBG,
       borderRadius: "0px 0px 60px 60px",
     },
